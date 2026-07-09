46235da81d97633c9be24b2986ca18a0d87eca1c
jsjjsj01 <jsjweb0@gmail.com>
2026-07-09 17:25:57 +0900

    feat: 검색 엔진 색인 생성을 위한 SEO 구성 개선

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import Sitemap from 'vite-plugin-sitemap';
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    Sitemap({
+      hostname: 'https://dockit.jsjweb0.workers.dev',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
